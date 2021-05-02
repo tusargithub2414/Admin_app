bdc7deb6ff773012c45e53711c3948012364447c
Tusar kanta mallick <tusarkanta.mallick@mindtraits.in>
2021-05-02 23:09:53 +0530

    Update feture

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,27 +14,44 @@ export class HeaderComponent implements OnInit {
   public showMyContainer = true;
   public grandChildren;
   menues: IMenu[];
-  subMenus:ISubMenu[];
-  childMenus:IChildMenu[];
+  public navigateFromMenue= false;
   constructor( private empluseService: EmpluseService, private router:Router, private sharedService : SharedService ) { }
 
   ngOnInit(): void {
     this.getMenu();
-    this.sharedService.currentGrandChildren.subscribe((res: any)=>{
-      this.grandChildren = res;
-    console.log('////', this.grandChildren);
-    })
   }
 
 getMenu(){
  this.empluseService.getLeftMenuCategories().subscribe((res) =>{
 console.log(res);
 this.menues= res;
+  this.openLeftMenu();
  })
 }
 gotToGrandChild(element: IChildMenu){
+  this.navigateFromMenue = true;
 this.sharedService.changeCurrentGrand(element);
   this.router.navigate(['../grandchildren']);
+}
+
+openLeftMenu(){
+
+  this.sharedService.currentGrandChildren.subscribe((res)=>{
+    if(!this.navigateFromMenue){
+      this.menues.forEach((element, index)=>{
+        element.children.forEach((childElement, index2) => {
+          const value =  childElement.children1.filter(element => element.ModuleName === res.ModuleName);
+          if(value[0]){
+            let clickelement: HTMLElement = document.getElementById('main_menue'.concat(index.toString())) as HTMLElement;
+            let clickChildelement: HTMLElement = document.getElementById('sub_main_menu'.concat(index2.toString()).concat(index.toString())) as HTMLElement;
+            clickelement.click();
+            clickChildelement.click();
+            clickChildelement.focus();
+          }
+        });
+      })
+    }
+   })
 }
 
 }
